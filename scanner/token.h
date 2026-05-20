19ca20251e97333ec6d53d63bd69bed58058837b
= <alexcros@mtu.edu>
2026-05-20 19:46:52 -0400

    feat: Added scanner module

---
+++ scanner/token.h
@@ -0,0 +1,25 @@
+#ifndef OATH_TOKEN_H
+#define OATH_TOKEN_H
+
+enum oath_lex_token {
+    VOID,
+    I8,
+    I16,
+    I32,
+    I64,
+    U8,
+    U16,
+    U32,
+    U64,
+    RETURN,
+    LEFT_PARAN,
+    RIGHT_PARAN,
+    LEFT_C_BRACE,
+    RIGHT_C_BRACE,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    SEMICOLON,
+    INTEGER
+};
+
+#endif // !OATH_TOKEN_H
